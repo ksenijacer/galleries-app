b4ca976d220d166ee6b327dfa9328d35f5a8e514
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-4.local>
2022-07-25 19:46:26 +0200

    added TymonJWT, Changed User model, added AuthController, Register Request, Password Check Rule, Added API routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User; 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class AuthController extends Controller
+{   
+
+    public function login(Request $request)
+     {
+         $credentials = $request->only(['email', 'password']);
+
+         if (! $token = Auth::attempt($credentials)) {
+             return response()->json(['message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+         }
+
+         return [
+             'token' => $token,
+             'user' => Auth::user()
+           ];
+     }
+
+    public function register(RegisterRequest $request)
+     {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+        $token = Auth::login($user);
+
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user(),
+        ],200);
+     }
+
+    public function getMyProfile()
+    {
+        $activeUser = Auth::user();
+        return response()->json($activeUser);
+    }
+
+
+    public function logout()
+    {
+        Auth::logout();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function refreshToken()
+    {
+        $token = Auth::refresh();
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+    public function unauthorizedRedirect()
+     {
+         return response()->json(['error' => 'Unauthorized'], 401);
+     }
+
+
+     protected function respondWithToken($token)
+     {
+         return response()->json([
+             'access_token' => $token,
+             'token_type' => 'bearer',
+             'expires_in' => auth()->factory()->getTTL() * 60
+         ]);
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
+Route::post('/register', [AuthController::class, 'register'])->middleware('guest');
+Route::get('/profile', [AuthController::class, 'getMyProfile'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('login', [AuthController::class, 'unauthorizedRedirect'])->name('login')->middleware('guest');
+Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+
